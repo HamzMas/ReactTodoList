dbd6f50af996ddfdf1d23a00e3e85c585ae0afc9
HamzMas <32570820+HamzMas@users.noreply.github.com>
2023-01-10 15:37:00 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import "./App.css";
+import TodoTable from "./components/TodoTable";
+
+function App() {
+  const [todos, setTodos] = useState([
+    { rowNumber: 1, rowDescription: "Feed cat", rowAssigned: "User One" },
+    { rowNumber: 2, rowDescription: "Water plants", rowAssigned: "User Two" },
+    { rowNumber: 3, rowDescription: "Make dinner", rowAssigned: "User One" },
+    { rowNumber: 4, rowDescription: "Charge phone", rowAssigned: "User One" },
+  ]);
+
+  const addTodo = () => {
+    if (todos.length > 0) {
+      const newTodo = {
+        rowNumber: todos.length + 1,
+        rowDescription: "New Todo",
+        rowAssigned: "User Three",
+      };
+      setTodos((todos) => [...todos, newTodo]);
+    }
+  };
+
+  return (
+    <div className="mt-5 container">
+      <div className="card">
+        <div className="card-header">Todo List</div>
+        <div className="card-body">
+          <TodoTable todos={todos} />
+          <button className="btn btn-primary" onClick={addTodo}>
+            Add new todo
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
