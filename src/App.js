e09c3c39d00b23c564975c2f3ba4709dfa72bc28
HamzMas <32570820+HamzMas@users.noreply.github.com>
2023-01-10 17:45:18 +0000

    Add custom todo items with form, and ability to delete items

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
 import "./App.css";
 import TodoTable from "./components/TodoTable";
+import NewTodoForm from "./components/NewTodoForm";
 
 function App() {
+  const [showAddTodoForm, setShowAddTodoForm] = useState(false);
+
   const [todos, setTodos] = useState([
     { rowNumber: 1, rowDescription: "Feed cat", rowAssigned: "User One" },
     { rowNumber: 2, rowDescription: "Water plants", rowAssigned: "User Two" },
@@ -10,15 +13,26 @@ function App() {
     { rowNumber: 4, rowDescription: "Charge phone", rowAssigned: "User One" },
   ]);
 
-  const addTodo = () => {
+  const addTodo = (description, assigned) => {
+    let rowNumber = 0;
     if (todos.length > 0) {
-      const newTodo = {
-        rowNumber: todos.length + 1,
-        rowDescription: "New Todo",
-        rowAssigned: "User Three",
-      };
-      setTodos((todos) => [...todos, newTodo]);
+      rowNumber = todos[todos.length - 1].rowNumber + 1;
+    } else {
+      rowNumber = 1;
     }
+    const newTodo = {
+      rowNumber: rowNumber,
+      rowDescription: description,
+      rowAssigned: assigned,
+    };
+    setTodos((todos) => [...todos, newTodo]);
+  };
+
+  const deleteTodo = (deleteTodoRowNumber) => {
+    let filtered = todos.filter(function (value) {
+      return value.rowNumber != deleteTodoRowNumber;
+    });
+    setTodos(filtered);
   };
 
   return (
@@ -26,10 +40,14 @@ function App() {
       <div className="card">
         <div className="card-header">Todo List</div>
         <div className="card-body">
-          <TodoTable todos={todos} />
-          <button className="btn btn-primary" onClick={addTodo}>
-            Add new todo
+          <TodoTable todos={todos} deleteTodo={deleteTodo} />
+          <button
+            className="btn btn-primary"
+            onClick={() => setShowAddTodoForm(!showAddTodoForm)}
+          >
+            {showAddTodoForm ? "Close New Todo" : "New Todo"}
           </button>
+          {showAddTodoForm && <NewTodoForm addTodo={addTodo} />}
         </div>
       </div>
     </div>
